0521026da956988f0cd2adee7c79470d0d33e838
MariaJenson96 <mariajenson1996@gmail.com>
2025-04-22 13:59:24 +0530

    ..

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,7 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Layout } from './pages/layout/_layout.component';
 
 export const routes: Routes = [
     {path:'', redirectTo:'Home', pathMatch:'full'},
-    {path:'Home', component:Layout, children:[
-     {path:'', component:DashboardComponent}   
-    ]}
+    {path:'Home', component:DashboardComponent }
 ];
